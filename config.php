ca898b25b30c58421f4c953eca285821b5d433b0
Patrick Thibaudeau <thibaud@yorku.ca>
2019-05-12 16:40:20 -0400

    Added local_buildings classes to config

--- config.php
+++ config.php
@@ -30,3 +30,27 @@ require_once($CFG->dirroot . '/local/roomsupport/classes/FaqAlerts.php');
 require_once($CFG->dirroot . '/local/roomsupport/classes/RaspberryPi.php');
 require_once($CFG->dirroot . '/local/roomsupport/classes/RaspberryPis.php');
 require_once($CFG->dirroot . '/local/roomsupport/classes/Statistics.php');
+
+/**
+ * Local Buildings
+ */
+require_once($CFG->dirroot . '/local/buildings/classes/Action.php');
+require_once($CFG->dirroot . '/local/buildings/classes/Asset.php');
+require_once($CFG->dirroot . '/local/buildings/classes/Assets.php');
+require_once($CFG->dirroot . '/local/buildings/classes/Base.php');
+require_once($CFG->dirroot . '/local/buildings/classes/Building.php');
+require_once($CFG->dirroot . '/local/buildings/classes/Buildings.php');
+require_once($CFG->dirroot . '/local/buildings/classes/Campus.php');
+require_once($CFG->dirroot . '/local/buildings/classes/Campuses.php');
+require_once($CFG->dirroot . '/local/buildings/classes/Faculties.php');
+require_once($CFG->dirroot . '/local/buildings/classes/Faculty.php');
+require_once($CFG->dirroot . '/local/buildings/classes/Floor.php');
+require_once($CFG->dirroot . '/local/buildings/classes/Floors.php');
+require_once($CFG->dirroot . '/local/buildings/classes/Helper.php');
+require_once($CFG->dirroot . '/local/buildings/classes/Room.php');
+require_once($CFG->dirroot . '/local/buildings/classes/RoomAssets.php');
+require_once($CFG->dirroot . '/local/buildings/classes/RoomType.php');
+require_once($CFG->dirroot . '/local/buildings/classes/RoomTypes.php');
+require_once($CFG->dirroot . '/local/buildings/classes/Rooms.php');
+require_once($CFG->dirroot . '/local/buildings/classes/SnipeIt.php');
+require_once($CFG->dirroot . '/local/buildings/classes/PDO.class.php');

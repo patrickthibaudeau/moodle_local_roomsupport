6c35be335e342fd11543bb54eedef69b463fe134
Patrick Thibaudeau <thibaud@yorku.ca>
2019-09-03 08:25:54 -0400

    Made devices list instead of card-deck

--- version.php
+++ version.php
@@ -30,7 +30,7 @@
  * ************************************************************************ */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2019051200;
+$plugin->version = 2019090300;
 $plugin->component = 'local_roomsupport';
 $plugin->release = '1.0.0';
 $plugin->requires = 2018051700;
